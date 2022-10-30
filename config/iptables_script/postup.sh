823d55b880811664e64fef57d8523c6e5d18c33f
yuruto-free <mailorder2receiver@gmail.com>
2022-10-31 00:59:07 +0900

    update shell scripts

--- config/iptables_script/postup.sh
+++ config/iptables_script/postup.sh
@@ -2,9 +2,9 @@
 
 readonly base_dir=$(cd $(dirname $0) && pwd)
 
-ls ${base_dir}/conf.up.d/*.conf | grep -v "^\W*$\|\s*#.*" | while read config_file; do
+ls ${base_dir}/conf.up.d/*.conf | while read config_file; do
     echo "[#] - ${config_file}"
-    cat ${config_file} | while read cmd; do
+    cat ${config_file} | grep -v "^\W*$\|\s*#.*" | while read cmd; do
         echo "[#]   ${cmd}"
         eval "${cmd}"
     done
